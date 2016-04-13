bb45cd2d769d85339598eb326c4ed380f16ee1dd
Breanna Maldonado <bmaldonado@ufl.edu>
2016-04-13 18:09:24 -0400

    Basic setup of Entity, Avatar, and Vehicle
    added some basic attributes and methods

--- src/models/entities/Avatar.java
+++ src/models/entities/Avatar.java
@@ -1,7 +1,22 @@
 package models.entities;
 
+import models.entities.occupation.*;
+import models.stats.CharacterStats;
 /**
- * Created by ben on 4/11/16.
+ * Created by Breanna on 4/13/16.
+ * This class represents the player controlled Entity in the game
+ * Subclass of Entity
  */
-public class Avatar {
+public class Avatar extends Entity{
+    //private Inventory inventory;
+    private Occupation occupation;
+    private CharacterStats avatarStats;
+    //private List<Skill> skillsList;
+
+    public Avatar(Occupation occupation) {
+        this.occupation = occupation;
+        occupation.initSkills(this);
+        occupation.initStats(this);
+        //this.inventory = new Inventory();
+    }
 }
