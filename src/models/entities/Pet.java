c4026343f689361458135de2fef82daa0b9e187c
David Peguero <pegdavid@gmail.com>
2016-04-14 18:28:06 -0400

    Added AI Observer to appropriate interaction handler

--- src/models/entities/Pet.java
+++ src/models/entities/Pet.java
@@ -4,8 +4,7 @@ import models.Map.Tile;
 import models.stats.LivingStats;
 
 import models.AI.PetBrain;
-
-
+import utilities.Point3D;
 
 
 /**
@@ -13,7 +12,7 @@ import models.AI.PetBrain;
  * This class represents the Pet in the game.
  */
 
-public class Pet extends Entity implements Movement, TileVisitable {
+public class Pet extends NPC implements Movement, TileVisitable {
     private LivingStats stats;
     private PetBrain petBrain;
 
@@ -48,4 +47,34 @@ public class Pet extends Entity implements Movement, TileVisitable {
     public LivingStats getStats() {
         return stats;
     }
+
+
+    //AI functions
+    public void makeMove(){
+        notifyMove(petBrain.generateMove());
+    }
+
+
+    //Add Observer
+    public void addObserver(Observer observer){
+        observers.add(observer);
+    }
+
+    //Observer Notifiers
+
+    @Override
+    public void notifyMove(Point3D point3D) {
+        for(Observer observer: observers){
+            observer.processMove(this, point3D);
+        }
+    }
+
+    @Override
+    public void notifyThought() {
+        for(Observer observer: observers){
+            observer.processThought(this);
+        }
+    }
+
+
 }
