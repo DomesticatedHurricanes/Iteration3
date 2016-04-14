4bc0bea7328d839b2bee39354caac383d3382189
mbentz <mbentz@ufl.edu>
2016-04-14 17:34:47 -0400

    Adds logic for handling moving regardless of current column

---
+++ src/Model/Direction/FindDirection.java
@@ -0,0 +1,69 @@
+package Model.Direction;
+
+import Model.Map.MapPoint;
+
+import java.awt.*;
+import java.util.HashMap;
+
+/**
+ * Created by Michael on 4/13/16.
+ */
+public class FindDirection {
+    private Direction direction;
+
+    public static HashMap<Direction, MapPoint> oddDirections;
+    public static HashMap<Direction, MapPoint> evenDirections;
+
+    public FindDirection(){
+        this.oddDirections = new HashMap<Direction, MapPoint>();
+        this.evenDirections = new HashMap<Direction, MapPoint>();
+        initializeMaps();
+    }
+
+    private void initializeMaps(){
+
+    }
+
+//    public MapPoint getAdjacent(Direction dir) {
+//        // even cases
+//        if () {
+//            return evenDirections.get(dir);
+//        }
+//        // odd cases
+//        return oddDirections.get(dir);
+//    }
+//    public void moveUpRight(){
+//        int x = currentX;
+//        int y = Math.abs(Math.cos((Math.PI*y)/2);
+//
+//        Point point = new point(x,y);
+//
+//        Avatar.move(point);
+//    }
+//
+//    public void moveUpLeft(){
+//        int x = currentX;
+//        int y = Math.abs(Math.cos((Math.PI*y)/2);
+//
+//        Point point = new point(x,y);
+//
+//        Avatar.move(point);
+//    }
+//
+//    public void moveDownLeft(){
+//        int x = currentX;
+//        int y = -1 * (Math.abs(Math.sin(Math.PI*y)/2));
+//
+//        Point point = new point(x,y);
+//
+//        Avatar.move(point);
+//    }
+//
+//    public void moveDownRight(){
+//        int x = currentx;
+//        int y = -1 * (Math.abs(Math.sin(Math.PI*y)/2));
+//
+//        Point point = new point(x,y);
+//    }
+}
+
