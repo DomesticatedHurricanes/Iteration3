73a8491d12b291651fe950535d67886080b5492d
kulakowskin <kulakowskin@yahoo.com>
2016-04-13 20:23:08 -0400

    Implemented AIBrain, MonsterBrain, and PetBrain

--- src/RunGame.java
+++ src/RunGame.java
@@ -1,6 +1,8 @@
 /**
  * Created by ben on 4/11/16.
  */
+import models.AI.MonsterBrain;
+import models.AI.PetBrain;
 import models.effects.RadialEffect;
 import utilities.Point3D;
 public class RunGame {
@@ -8,5 +10,9 @@ public class RunGame {
         System.out.println("Hello World");
         Point3D point = new Point3D(0,0,3);
         new RadialEffect(point, 2);
+
+        PetBrain PB = new PetBrain();
+        PB.decideToSteal();
+
     }
 }
