f2908e5aa565f1dfea3e662e732e30f6b8d85f82
benchen88888888 <benchen88888888@gmail.com>
2016-04-17 01:32:40 -0400

    Added passive skill interface and implementation details

--- src/models/Skills/ObservationSkill.java
+++ src/models/Skills/ObservationSkill.java
@@ -1,13 +1,46 @@
 package models.Skills;
 
+import models.effects.RadialEffect;
+import models.entities.Avatar;
 import models.entities.Entity;
 import models.stats.CharacterStats;
+import models.stats.Stats;
+import utilities.Point3D;
+
+import java.util.ArrayList;
 
 /**
  * Created by Breanna on 4/13/16.
  */
-public class ObservationSkill extends Skill {
+public class ObservationSkill extends SearchableSkill implements PassiveSkill{
     private Entity target;
+
+    public ArrayList<Stats> useObservation(Avatar avatar){
+        ArrayList<Point3D> affectedPoints = search();
+        ArrayList<Entity> victims = findVictims(affectedPoints);
+        ArrayList<Stats> statsArrayList= new ArrayList<>();
+        for(Entity victim:victims){
+            Stats victimStats = victim.getStats();
+            statsArrayList.add(victimStats);
+        }
+        return statsArrayList;
+    }
+
+    @Override
+    public void onUpdate(Avatar avatar) {
+        useObservation(avatar);//this returns an arraylist of stats for views
+    }
+
+    @Override
+    public ArrayList<Point3D> search() {
+        Point3D point = new Point3D(0,0,0);
+        RadialEffect radialEffect = new RadialEffect(point,3);
+        ArrayList<Point3D> affectedPoints;
+        affectedPoints=radialEffect.getTilePoints();
+
+        return affectedPoints;
+    }
+
 
     public CharacterStats sendData() { return new CharacterStats(); }
 }
---
+++ src/models/Skills/PassiveSkill.java
@@ -0,0 +1,10 @@
+package models.Skills;
+
+import models.entities.Avatar;
+
+/**
+ * Created by ben on 4/17/16.
+ */
+public interface PassiveSkill {
+    void onUpdate(Avatar avatar);
+}
